5030f5b5539fe29cb90ee68807f5ae9fcb16389b
Ayush Dhar <ayush_dhar@outlook.com>
2023-04-23 12:36:08 -0400

    initial

--- src/config.py
+++ src/config.py
@@ -2,6 +2,11 @@ from pydantic import BaseSettings
 
 
 class Config(BaseSettings):
+    """_summary_
+
+    Args:
+        BaseSettings (_type_): _description_
+    """
     case_tracker_url: str
     case_status_to_monitor: str
     notify_topic_arn: str
